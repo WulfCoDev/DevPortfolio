7016f17d5bd64f01b4fc31bfb29576a258e7ec0b
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2024-02-02 17:13:00 -0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center">
+      {/* <Navbar /> */}
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-white font-quad text-2xl">Hello</h1>
+      <div className="h-[200px] w-[200px]">
+        <img
+          className="rounded-xl shadow-xl shadow-[#292f2b]"
+          src="src/assets/hero.JPG"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
