6b458e9c9afb9cac7674bc40f7b34df8cdf66589
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2024-02-03 20:28:26 -0800

    text css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
           "radial-gradient(circle, rgba(169,213,242,0.75) 0%, rgba(0,0,0,0) 55%)",
       }}
     >
-      <h1 className="text-[#dae2df] font-quad text-4xl mb-2">Hello</h1>
-      <p className="font-tertiary caption-top">My name is</p>
-
+      <h1 className="text-secondary font-quad text-4xl mb-2">Hello</h1>
+      <p className="font-tertiary text-primary">My name is</p>
+      <p className="font-quad text-secondary text-4xl ">Christian Alexander</p>
       <div className="h-[150px] w-[150px] m-2">
         <img
           className="rounded-xl shadow-xl shadow-[#292f2b]"
@@ -17,7 +17,6 @@ const Hero = () => {
           alt=""
         />
       </div>
-      <p className="font-quad text-primary text-4xl ">Christian Alexander</p>
     </div>
   );
 };
